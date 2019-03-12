126b10a1c50e4ddc854b6c4a6b59e6c3ba94e115
Trollicorn <muddin3@stuy.edu>
2019-03-12 17:23:38 -0400

    b

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ color = [ 0, 127, 255 ]
 edges = []
 transform = new_matrix()
 ident(transform)
+"""
+f = open("gif.sh",'w')
+f.write("#!/bin/bash\n\n")
+f.write("convert -delay 0.1 -loop 0 ")
+for i in range(110):
+    f.write("use"+str(i)+".png ")
+f.write("art.gif")
+"""
 
 parse("script",edges,transform,screen,color)
--- maingood.py
+++ maingood.py
@@ -4,6 +4,7 @@ from matrix import *
 from parse import *
 from transform import *
 from math import cos,sin,tan,radians
+from os import remove
 
 for i in range(110):
     screen = new_screen()
@@ -16,11 +17,12 @@ for i in range(110):
     if i < 56:
         f.write(str(190-i)+" 325 0 10\ncircle\n"+str(340-i)+" 325 0 10\n")
     else:
-        f.write(str(135+(i-55)+" 325 0 10\ncircle\n"+str(340-i)+" 325 0 10\n")
-    f.write("hermite\n150 150 350 150 -100 -100 100 150\nbezier\n126 323 127 355 194 355 200 324\n"
+        f.write(str(135+(i-55))+" 325 0 10\ncircle\n"+str(340-i)+" 325 0 10\n")
+    f.write("hermite\n150 150 350 150 -100 -100 100 150\nbezier\n126 323 127 355 194 355 200 324\n")
     f.write("bezier\n276 323 277 355 344 355 350 324\nline\n250 275 0 250 325 0\nbezier\n250 215 300 195 300 295 250 275\n")
-    f.write("\nsave\nuse"+str(i)+".png")
+    f.write("apply\nsave\nuse"+str(i)+".png\n")
     f.close()
     parse("epic"+str(i)+".txt",edges,transform,screen,color)
+#    remove("epic"+str(i)+".txt")
 
 #parse("good",edges,transform,screen,color)
