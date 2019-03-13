ff47c347017791d544ea941f119ef180a4301832
Mohammed Uddin <muddin3@stuy.edu>
2019-03-13 00:46:52 -0400

    final push

--- maingood.py
+++ maingood.py
@@ -5,7 +5,7 @@ from parse import *
 from transform import *
 from math import cos,sin,tan,radians
 from os import remove
-
+#""" delete/add # to enable/disable
 for i in range(110):
     screen = new_screen()
     color = [ 0, 127, 255 ]
@@ -15,14 +15,20 @@ for i in range(110):
     f = open("epic"+str(i)+".txt",'w')
     f.write("circle\n250 250 0 200\ncircle\n")
     if i < 56:
-        f.write(str(190-i)+" 325 0 10\ncircle\n"+str(340-i)+" 325 0 10\n")
+        f.write(str(200-i)+" 325 0 10\ncircle\n"+str(350-i)+" 325 0 10\n")
     else:
-        f.write(str(135+(i-55))+" 325 0 10\ncircle\n"+str(340-i)+" 325 0 10\n")
-    f.write("hermite\n150 150 350 150 -100 -100 100 150\nbezier\n126 323 127 355 194 355 200 324\n")
-    f.write("bezier\n276 323 277 355 344 355 350 324\nline\n250 275 0 250 325 0\nbezier\n250 215 300 195 300 295 250 275\n")
+        f.write(str(145+(i-55))+" 325 0 10\ncircle\n"+str(295+(i-55))+" 325 0 10\n")
+    f.write("hermite\n150 150 350 150 -100 -100 100 150\nbezier\n136 323 137 355 204 355 210 324\n")
+    f.write("bezier\n286 323 287 355 354 355 360 324\nline\n250 275 0 250 325 0\nbezier\n250 215 300 195 300 295 250 275\n")
     f.write("apply\nsave\nuse"+str(i)+".png\n")
     f.close()
     parse("epic"+str(i)+".txt",edges,transform,screen,color)
-#    remove("epic"+str(i)+".txt")
-
-#parse("good",edges,transform,screen,color)
+    remove("epic"+str(i)+".txt")
+"""
+screen = new_screen()
+color = [ 0, 127, 255 ]
+edges = []
+transform = new_matrix()
+ident(transform)
+#"""
+parse("good",edges,transform,screen,color)
